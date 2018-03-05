cfc042690abd51214e9de9b00b5c430f9a6e565f
hanweida <534446340@qq.com>
2018-03-05 16:47:24 +0800

    修改NBA赛事列表样式

---
+++ src/component/home/hometab/Zhibo_Source.js
@@ -0,0 +1,27 @@
+'use strict';
+import React, {Component} from 'react';
+import {
+    FlatList,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Alert,
+    TouchableOpacity,
+    NativeModules,
+    Dimensions,
+    SectionList
+} from 'react-native';
+
+export default class Zhibo_Source extends React.PureComponent{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Zhibo_Source
+                </Text>
+            </View>
+        );
+    }
+}
