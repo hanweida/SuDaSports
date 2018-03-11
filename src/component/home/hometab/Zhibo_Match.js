4668537983d0f1bcad82f566c4ac76b0bbb8e713
yingmule <534446340@qq.com>
2018-03-11 23:07:09 +0800

    修改react-navitation 返回界面

--- src/component/home/hometab/Zhibo_Match.js
+++ src/component/home/hometab/Zhibo_Match.js
@@ -44,7 +44,8 @@ export default class Zhibo_Match extends React.PureComponent{
     }
 
     onTabPress(item, mid){
-        NativeModules.WebviewRNModule.show("http://192.168.100.104:8080/video/getnbaurl.biz?url="+item.sourceValue+"&"+"mid="+mid+"&"+"sourceName="+item.sourceName);
+        NativeModules.WebviewRNModule.show("http://192.168.1.13:8080/video/getnbaurl.biz?url="+item.sourceValue+"&"+"mid="+mid+"&"+"sourceName="+item.sourceName);
+        //NativeModules.WebviewRNModule.show("http://120.78.150.194:8080/biz/test.html");
     }
     /** 渲染视图数据*/
     renderLoadingView(){
@@ -88,8 +89,9 @@ export default class Zhibo_Match extends React.PureComponent{
         const {state} = navi;
 
         var list = [];
-        fetch('http://192.168.100.104:8080/gamedata/matchStat.biz?' +
-            'mid='+state.params.item.mid+"&tabType=2&homeTeamName="+state.params.item.home_team+"&guestTeamName="+state.params.item.guest_team)
+        const matchStatUrl = "http://192.168.1.13:8080/gamedata/matchStat.biz?mid="+state.params.item.mid+"&tabType=2&homeTeamName="+state.params.item.home_team+"&guestTeamName="+state.params.item.guest_team;
+        //const matchStatUrl = "http://192.168.1.13:8080/gamedata/matchStat.biz?mid="+state.params.item.mid+"&tabType=2&homeTeamName=独行侠&guestTeamName=灰熊";
+        fetch(matchStatUrl)
         //var responses = fetch('http://192.168.100.104:8080/video/gamenbalist.biz')
             .then((response) => response.json())
             //获得返回的json
